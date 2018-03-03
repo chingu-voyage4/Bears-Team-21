24897cdf246db51be8750d766d98a5576b3602bc
Dan <jadczak.d@gmail.com>
2018-03-03 21:15:17 +0100

    dj removed socket duplication

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:4444');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
