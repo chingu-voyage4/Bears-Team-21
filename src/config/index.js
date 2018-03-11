40cd7aaa7a5500354cc9a183a2f031c183b2dd5a
Asjas <asjas@outlook.com>
2018-03-11 10:16:20 +0200

    add mail and cookie env keys to config

--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,12 @@ dotenv.config()
 const config = {
   'port': process.env.PORT || 3333,
   'db': process.env.DB,
-  'portIO': process.env.PORTIO || 4444
+  'portIO': process.env.PORTIO || 4444,
+  'cookieKey': process.env.COOKIEKEY,
+  'mail_host': process.env.MAIL_HOST,
+  'mail_port': process.env.MAIL_PORT,
+  'mail_user': process.env.MAIL_USER,
+  'mail_pass': process.env.MAIL_PASS
 }
 
 export default config
